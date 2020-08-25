ede8a7d221a08b97c876ac07166ed2a1e1323971
biFebriansyah <bukanebi@gmail.com>
2020-08-25 14:16:52 +0700

    add users data

---
+++ user.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        id: 01,
+        username: "bukanebi",
+        password: "passwordkuat",
+    },
+    {
+        id: 02,
+        username: "bukanebi",
+        password: "passwordkuat",
+    },
+    {
+        id: 03,
+        username: "bukanebi",
+        password: "passwordkuat",
+    },
+]
